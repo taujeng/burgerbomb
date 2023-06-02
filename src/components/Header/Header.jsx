8e283183207c339022b581daec92ee90fadd14e0
Tau Jeng <taujeng.1@gmail.com>
2023-06-02 14:30:07 -0700

    Add Footer Links, redid Logo so that Burger Bomb is one line

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,31 +4,12 @@ import "./header.css"
 
 const Header = () => {
   return (
-        // <Container fluid>
-        //     <Navbar sticky="top" >
-        //             <Row className="nav-row">
-        //                 <Col className="nav-left col-4">
-        //                     <Navbar.Text className="nav-text">Menu</Navbar.Text>
-        //                     <Navbar.Text className="nav-text">Locations</Navbar.Text>
-        //                     <Navbar.Text className="nav-text">Careers</Navbar.Text>
-        //                 </Col>
-        //                 <Col className="">
-        //                     <Navbar.Brand href="https://burgerbomb.com">
-        //                         <img src="./images/logo.svg" alt="Burger Bomb" id="bomb"/>
-        //                     </Navbar.Brand>
-        //                 </Col>
-        //                 <Col className="nav-right col-4">
-        //                     <Navbar.Text className="nav-text">Gift Cards</Navbar.Text>
-        //                     <Navbar.Text className="nav-text">Order Now</Navbar.Text>
-        //                 </Col>
-        //             </Row>
-
-        //     </Navbar>
-        // </Container>
         <Navbar collapseOnSelect expand="lg" className="justify-content-center">
             <Container className="">
                 <Navbar.Brand href="https://burgerbomb.com" className='mx-auto'>
-                    <img src="./images/logo.svg" alt="Burger Bomb" id="bomb"/>
+                    {/* <img src="./images/logo.svg" alt="Burger Bomb" id="bomb"/> */}
+                    <h1>Burger Bomb</h1>
+                    <span><img src="/images/bomb.PNG" id="bomb-logo" alt="Bomb" /></span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-control="responsive-nav" />
                 <Navbar.Collapse id="responsive-nav" className="justify-content-end">
