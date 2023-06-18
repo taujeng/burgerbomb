22cc68b59656075afe60f41750568cfeb0099e90
Tau Jeng <taujeng.1@gmail.com>
2023-06-18 16:12:36 -0700

    Fixed bug caused by useContext syntax. Add to Order button for Choices works now

--- src/pages/Menu/Burgers/Burgers.jsx
+++ src/pages/Menu/Burgers/Burgers.jsx
@@ -13,6 +13,7 @@ import { PlusCircle, DashCircle } from "react-bootstrap-icons";
 
 const Burgers = () => {
     const { order, setOrder } = useContext(OrderContext)
+    
     const [burgerPrice, setBurgerPrice] = useState(0);
     const [burgerType, setBurgerType] = useState(false);
     const [bunChoice, setBunChoice] = useState(false);
@@ -122,8 +123,7 @@ const Burgers = () => {
             </div>
             <div className="burger-addToOrder">
                 <button onClick={addOrder} disabled={!burgerType || !bunChoice} 
-                className={(!burgerType || !bunChoice) ? "disabled-btn" : "active-btn"}><b>Add to Order</b></button>
-
+                className={(!burgerType || !bunChoice) ? "disabled-btn" : "active-btn"}><b>Add to Order</b></button> 
             </div>
         </div>   
     )
