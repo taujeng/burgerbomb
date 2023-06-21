2a516ebf0a7ef9e06f6a4450657a4e3fe848293b
Tau Jeng <taujeng.1@gmail.com>
2023-06-21 01:33:33 -0700

    Landing page and Header more mobile friendly

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,11 +6,25 @@ import "./landing.css"
 const Landing = () => {
   return (
       <div className="landing-container">
-        <div className="landing-text">
-            <div><i>Bursting with flavor..</i></div>
-            <LinkContainer to="/menu">
-              <Button className="landing-btn">Order Now</Button>
-            </LinkContainer>
+        <div className="landing-hero">
+          <img className="landing-img" src="./images/hamburger.jpg" alt="" />
+          <div className="landing-text">
+              <div><i>Custom-made burgers</i></div>
+              <div><i>bursting with flavor..</i></div>
+              <LinkContainer to="/menu">
+                <Button className="landing-btn">Order Now</Button>
+              </LinkContainer>
+          </div>
+        </div>
+        <div className="landing-two">
+          <div className="landing-left">
+            <img src="./images/unused/veg-vert.png" alt="" />
+          </div>
+          <div className="landing-right">
+            <img src="./images/unused/veg-hori.jpg" className="landing-mobile-veg" alt="" />
+            <h2>Coriander açai spicy chili</h2>
+            <p>pepper vine tomatoes lemon red lentil soup avocado dark and stormy crispy iceberg lettuce mocha chocolate black beans double dark chocolate chilies leek banh mi salad rolls coconut milk Thai sun.</p>
+          </div>
         </div>
       </div>
   )
