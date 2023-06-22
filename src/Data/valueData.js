827cf688e21ef5005683fe1d127ad8229d5623ef
Tau Jeng <taujeng.1@gmail.com>
2023-06-22 01:57:07 -0700

    Update landing page descriptions, add some potential text for the Value page

---
+++ src/Data/valueData.js
@@ -0,0 +1,13 @@
+// Introducing Burger Bomb, where every bite bursts with flavor! We take pride in crafting custom-made burgers using only the freshest ingredients sourced from local farmers. Our commitment to quality starts from the ground up, as we carefully select the finest produce and meat, ensuring that each ingredient is at its peak of freshness.
+
+// By partnering with local farmers, we support our community and promote sustainability. We believe in the power of farm-to-table dining, where you can taste the difference in every bite. From crisp lettuce and ripe tomatoes to juicy beef and tender chicken, our ingredients are thoughtfully chosen to create a mouthwatering experience you won't forget.
+
+// At Burger Bomb, we value the connection between food and the land it comes from. Our dedication to using locally sourced ingredients not only guarantees exceptional taste but also reduces our carbon footprint. It's a win-win for your taste buds and the environment!
+
+// Experience the delight of savoring burgers made with care and passion. Order now and discover the irresistible flavors that come from using fresh, locally sourced ingredients. Treat yourself to a burger experience that embodies the essence of farm-fresh goodness. Welcome to Burger Bomb, where every bite is a taste of culinary excellence!
+
+
+// another:
+// Burger Bomb proudly collaborates with nearby farmers to bring you the most amazing organic ingredients. We believe that great-tasting burgers start with exceptional ingredients, which is why we go the extra mile to source them from local farms. Our partnership with farmers ensures that every burger you enjoy at Burger Bomb is bursting with the natural flavors and goodness that only fresh, locally sourced ingredients can provide.
+
+// By working closely with local farmers, we support sustainable agriculture practices and promote the local economy. It's our way of delivering a truly farm-to-table experience, where each bite is a testament to our commitment to quality and taste. Taste the difference that locally sourced ingredients make and indulge in the mouthwatering flavors that define Burger Bomb.
